a7d46f8adc3767b8f585dc8138f23a76a95aa341
Hardik Paliwal <hardikpaliwal1999@gmail.com>
2023-05-22 07:57:33 -0400

    add images

--- imageParseScript.py
+++ imageParseScript.py
@@ -1,10 +1,9 @@
 import os
 from PIL import Image
-from PIL.ExifTags import TAGS
+from PIL.PngImagePlugin import PngInfo
 import json
 
-directory = './'
-
+directory = 'G:/My Drive/ai art/finished'
 outputDirectory = "./testImages/images"
 images = []
 for subdir, dirs, files in os.walk(directory):
@@ -13,16 +12,22 @@ for subdir, dirs, files in os.walk(directory):
             images.append(os.path.join(subdir, file))
     # read the image data using PIL
 
-
+imagesMetaData= {}
 for imagePath in images:
-    image = Image.open(imagePath)
+    try:
+        image = Image.open(imagePath)
+        metadata = {}
+        toParse = image.text["parameters"]
+        metadata["Prompt"] = toParse.split('\n')[0]
+        metadata["Negative Prompt"] = toParse.split('\n')[1][16:]
+        lastRow = toParse.split('\n')[2]
+        metadata["Steps"] = lastRow.split(': ')[1].split(',')[0]
+        metadata["Sampler"] = lastRow.split(': ')[2].split(',')[0]
+        metadata["CFG scale"] = lastRow.split(': ')[3].split(',')[0]
+        metadata["Seed"] = lastRow.split(': ')[4].split(',')[0]
+        metadata["Model"] = lastRow.split(': ')[7].split(',')[0]
+        metadata["Denoising strength"] = lastRow.split(': ')[8].split(',')[0]
 
-    # extract EXIF data
-    exifdata = image.getexif()
-
-    # iterating over all EXIF data fields
-    for tag_id in exifdata:
-        # get the tag name, instead of human unreadable tag id
-        tag = TAGS.get(tag_id, tag_id)
-        data = exifdata.get(tag_id).decode("utf-16")
-        print(f"{tag:25}: {data}")  
+        print(json.dumps(metadata))
+    except:
+        pass
